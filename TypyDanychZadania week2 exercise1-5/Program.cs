e4f89182471f13eddc71e30084f62b2e0cc79385
Ted K <ted.k07@gmail.com>
2023-02-06 22:23:04 +0100

    Add project files.

---
+++ TypyDanychZadania week2 exercise1-5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TypyDanychZadania_week2_exercise1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Typy danych week2, lesson 4, exercise 1- 5
+            // Zadanie 1
+
+            string name;
+            string fullName;
+            int age;
+            char sex;
+            string pesel;
+            int number;
+
+            Console.WriteLine("Program ten zbiera następujące dane o pracownikach: imię, nazwisko, wiek, płeć, PESEL, numer pracownika");
+            Console.ReadKey();
+        }
+    }
+}
